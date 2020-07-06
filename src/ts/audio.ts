92943a1cef913c02eee26f860b0ffc93ac04ac87
jacekwozniak12 <jac3kw0zniak@gmail.com>
2020-07-06 13:15:14 +0200

    Stopping sound

--- src/ts/audio.ts
+++ src/ts/audio.ts
@@ -1,35 +1,51 @@
-import { PolySynth, Synth, Transport, Part } from 'Tone';
-import { Note, Chord } from "./definitions";
+import { Destination, PolySynth, Synth, Transport, Part } from 'Tone';
+import { Note, Chord, Options } from "./definitions";
 
 export class Audio {
 
     private instrument: PolySynth
     private chord: Chord;
+    private options: Options;
+    private part: Part;
 
     constructor() {
         this.setup();
+        this.part = new Part();
     }
 
-    updateChord(chord: Chord) {
-        this.chord = chord;
+    setOptions(options : Options){
+        return this.options = options;
     }
 
-    getChord(){
+    updateChord(chord: Chord): Chord {
+        return this.chord = chord;
+    }
+
+    setChord(chord): Chord {
+        return this.chord = chord;
+    }
+
+    getChord(): Chord {
         return this.chord;
     }
 
-    addNote(note: Note) {
+    addNote(note: Note): Chord {
         this.chord.notes.unshift(note);
+        return this.chord;
     }
 
-    deleteNote(note: Note) {
+    deleteNote(note: Note): Chord {
+        this.chord.notes = this.chord.notes.filter(x => x.fretboardPosition != note.fretboardPosition);
+        return this.chord;
+    }
+
+    deleteAllNoteInstances(note: Note): Chord {
         this.chord.notes = this.chord.notes.filter(x => x.name != note.name);
+        return this.chord;
     }
 
-    setup() : this{
-
+    setup(): this {
         this.chord = new Chord([])
-
         this.instrument = new PolySynth(
             Synth,
             {
@@ -42,41 +58,41 @@ export class Audio {
         return this;
     }
 
-    dispose() {
-        Transport.stop();
-        Transport.cancel();
+    dispose(): void {
+        this.stop();
         this.instrument.dispose();
         this.instrument = null;
     }
 
-    play(chord: Chord = this.chord) {
+    play(chord: Chord = this.chord): void {
         Transport.stop();
         Transport.cancel();
-
         let temp = this.instrument;
-
-        let part = new Part(function (time, event) {
+        this.part = new Part(function (time, event) {
             temp.triggerAttackRelease(
                 event.note,
                 event.dur,
                 time,
                 event.volume)
-        }, []
-        )
-
+        }, [])
         chord.notes.forEach(e => {
-            part.add(e.options.delay,
+            this.part.add(e.options.delay,
                 {
                     note: e.name,
                     dur: e.options.duration,
-                    volume: e.options.volume
+                    volume: e.options.volume * this.options.volume
                 });
         });
-
-        part.start(0);
-        part.loop = 0
-
+        Destination.mute = false;
+        this.part.start(0);
+        this.part.loop = 0
         Transport.start();
+    }
+
+    stop(){
+        Destination.mute = true;
+        Transport.stop();
+        Transport.cancel();
     }
 }
 
